1ffb2a27cf870b43ea59cd47bd2e44d0002508c7
surajseth90 <surajseth90@gmail.com>
2021-07-03 11:41:11 +0530

    Added Employee payroll form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import PayrollForm from './components/payroll-form/payroll-form'
+import {
+  BrowserRouter as Router,Switch,Route,Redirect
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <Router>
+        <Switch>
+          <Route exact path="">
+              <PayrollForm />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
